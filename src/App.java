ab256b15eec193f25c517abf87897d76c303c356
Sergiibut05 <sergiibutrii555@gmail.com>
2024-11-03 22:52:27 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import Ejercicio12.Ejercicio12;
+import Ejercicio13.Ejercicio13;
+import Ejercicio14.Ejercicio14;
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*Ejercicio12.ejercicio12();*/
+        /*Ejercicio13.ejercicio13();*/
+        Ejercicio14.ejercicio14();
+    }
+}
