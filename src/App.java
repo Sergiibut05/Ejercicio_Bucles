c16995f23fa12b293c941357a6e4ed9e4e51bb41
Sergiibut05 <sergiibutrii555@gmail.com>
2024-11-06 13:20:09 +0100

    commit 2

--- src/App.java
+++ src/App.java
@@ -5,14 +5,22 @@ import Ejercicio22.Ejercicio22;
 import Ejercicio24.Ejercicio24;
 import Ejercicio25.Ejercicio25;
 import Ejercicio26.Ejercicio26;
+import Ejercicio27.Ejercicio27;
+import Ejercicio28.Ejercicio28;
+import Ejercicio29.Ejercicio29;
+import Ejercicio30.Ejercicio30;
 public class App {
     public static void main(String[] args) throws Exception {
         /*Ejercicio12.ejercicio12();*/
         /*Ejercicio13.ejercicio13();*/
         /*Ejercicio14.ejercicio14();*/
         /*Ejercicio22.ejercicio22();*/
-        Ejercicio24.ejercicio24();
+        /*Ejercicio24.ejercicio24();*/
         /*Ejercicio25.ejercicio25();*/
-        /*Ejercicio26.ejercicio26();*/
+        //Ejercicio26.ejercicio26();
+        //Ejercicio27.ejercicio27();
+        //Ejercicio28.ejercicio28();
+        //Ejercicio29.ejercicio29();
+        Ejercicio30.ejercicio30();
     }
 }
